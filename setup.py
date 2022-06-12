3ab2c474bfec40610682ed687bec3119d0d36ae8
Srikanth Gedela <ubuntu22@SriDellXPS15>
2022-06-12 13:27:10 +0100

    Adding cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Based on the data available, this project predicts whether or not a customer will switch telecommunications providers ("customer churn predicition").',
+    author='Srikanth-Gedela',
+    license='MIT',
+)
